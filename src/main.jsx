1e331d98acd7d770d2db70050e079ea778eb3b6c
Abdo Shajadi <abd.shajadi@gmail.com>
2016-06-23 01:59:30 +0430

    routers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ListManager = require('./components/ListManager.jsx');
+var Routes = require('./Routes.jsx');
 
-ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
-
-ReactDOM.render(<ListManager title="Car" />, document.getElementById('car'));
-
-ReactDOM.render(<ListManager title="Home" headingColor="#00ff00" />, document.getElementById('home'));
-
+ReactDOM.render(Routes, document.getElementById('main'));
